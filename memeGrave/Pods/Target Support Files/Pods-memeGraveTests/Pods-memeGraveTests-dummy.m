fb3e71e89b917ceb9a8c6fd02ed10a6b7d5a4e17
Juno Yu <jxy35@hitit.xyz>
2020-06-22 22:13:12 +0900

    firebase google login added

---
+++ memeGrave/Pods/Target Support Files/Pods-memeGraveTests/Pods-memeGraveTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_memeGraveTests : NSObject
+@end
+@implementation PodsDummy_Pods_memeGraveTests
+@end
---
+++ memeGrave/memeGrave/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  memeGrave
+//
+//  Created by Yu Juno on 2020/06/22.
+//  Copyright © 2020 memeGrave. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController:UIViewController {
+	//MARK: - Properties
+	//MARK: - Init
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .red
+	}
+	//MARK: - Handlers
+	//MARK: - Constraints
+}
